141c0522c3605624cedae7a4c12ed5ced08bdd8e
RomainCostes <44799984+RomainCostes@users.noreply.github.com>
2023-01-05 11:19:16 +0100

    fix service worker

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    registerServiceWorker();
+
+    var status = document.getElementById("status");
+
+    function updateOnlineStatus(event) {
+        var condition = navigator.onLine ? "online" : "offline";
+
+        status.className = condition;
+        status.innerHTML = condition.toUpperCase();
+    }
+    window.addEventListener("online", updateOnlineStatus);
+    window.addEventListener("offline", updateOnlineStatus);
+});
+
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
-      try {
-        const registration = await navigator.serviceWorker.register("/sw.js", {
-          scope: "/",
-        });
-        if (registration.installing) {
-          console.log("Installation du service worker en cours");
-        } else if (registration.waiting) {
-          console.log("Service worker installé");
-        } else if (registration.active) {
-          console.log("Service worker actif");
+        try {
+            const registration = await navigator.serviceWorker.register(
+                "https://vincentdm31.github.io/pwa/sw.js",
+                {
+                    scope: "https://vincentdm31.github.io/pwa/",
+                }
+            );
+            if (registration.installing) {
+                console.log("Installation du service worker en cours");
+            } else if (registration.waiting) {
+                console.log("Service worker installé");
+            } else if (registration.active) {
+                console.log("Service worker actif");
+            }
+        } catch (error) {
+            console.error(`L'enregistrement a échoué : ${error}`);
         }
-      } catch (error) {
-        console.error(`L'enregistrement a échoué : ${error}`);
-      }
     }
-  };
-// …
-
-registerServiceWorker();
+};
---
+++ sw.js
@@ -0,0 +1,19 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/pwa/",
+            "/pwa/index.html",
+            "/pwa/index.css",
+            "/pwa/app.js",
+            "/pwa/cat-48.png",
+            "/pwa/cat.png",
+        ])
+    );
+});
+
+self.addEventListener("fetch", () => console.log("fetch"));
